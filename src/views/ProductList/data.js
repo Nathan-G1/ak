6d196e3d90711c423cfbdca0218abfd61b19726c
ermias111 <ermimichael@gmail.com>
2020-06-29 23:47:19 +0300

    courses title changed for demonstration

--- src/views/ProductList/data.js
+++ src/views/ProductList/data.js
@@ -4,7 +4,7 @@ export default [
   {
     id: uuid(),
     rating: 5,
-    title: 'Dropbox',
+    title: 'Biology in Amharic',
     time: 3,
     description:
       'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
@@ -15,7 +15,7 @@ export default [
   {
     id: uuid(),
     rating: 5,
-    title: 'Medium Corporation',
+    title: 'Customers or Buyers Expressions',
     time: 8,
     description:
       'Medium is an online publishing platform developed by Evan Williams, and launched in August 2012.',
@@ -26,7 +26,7 @@ export default [
   {
     id: uuid(),
     rating: 5,
-    title: 'Slack',
+    title: 'English for Beginners',
     time: 9,
     description:
       'Slack is a cloud-based set of team collaboration tools and services, founded by Stewart Butterfield.',
@@ -37,7 +37,7 @@ export default [
   {
     id: uuid(),
     rating: 5,
-    title: 'Lyft',
+    title: 'Chemistry in Amharic',
     time: 3,
     description:
       'Lyft is an on-demand transportation company based in San Francisco, California.',
@@ -48,7 +48,7 @@ export default [
   {
     id: uuid(),
     rating: 5,
-    title: 'GitHub',
+    title: 'Business English',
     time: 8,
     description:
       'GitHub is a web-based hosting service for version control of code using Git.',
@@ -59,7 +59,7 @@ export default [
   {
     id: uuid(),
     rating: 5,
-    title: 'Squarespace',
+    title: 'Maths in Amharic',
     time: 3,
     description:
       'Squarespace provides software as a service for website building and hosting. Headquartered in NYC.',
